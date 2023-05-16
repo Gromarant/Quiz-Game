2e3c49d11e4c5b331d463d1b31a911aea4fbeb0f
Mariangelica Rodriguez <gromarant@gmail.com>
2023-05-16 20:01:49 +0200

    Set data structure

---
+++ index.js
@@ -0,0 +1,53 @@
+let questions = [
+  {
+    statement: 'Pregunta1',
+    answers: [
+      { text: 'respuesta1', for: 'q1AnswerOne', nameAttribute: 'questionOne', correct: true },
+      { text: 'respuesta2', for: 'q1AnswerTwo', nameAttribute: 'questionOne', correct: false },
+      { text: 'respuesta3', for: 'q1AnswerThree', nameAttribute: 'questionOne', correct: false },
+      { text: 'respuesta4', for: 'q1Answerfour', nameAttribute: 'questionOne', correct: false },
+    ]
+  },
+  {
+    statement: 'Pregunta2',
+    answers: [
+      { text: 'respuesta1', for: 'q1AnswerOne', nameAttribute: 'questionTwo', correct: true },
+      { text: 'respuesta2', for: 'q1AnswerTwo', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta3', for: 'q1AnswerThree', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta4', for: 'q1Answerfour', nameAttribute: 'questionTwo', correct: false },
+    ]
+  },
+  {
+    statement: 'Pregunta2',
+    answers: [
+      { text: 'respuesta1', for: 'q1AnswerOne', nameAttribute: 'questionTwo', correct: true },
+      { text: 'respuesta2', for: 'q1AnswerTwo', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta3', for: 'q1AnswerThree', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta4', for: 'q1Answerfour', nameAttribute: 'questionTwo', correct: false },
+    ]
+  }, {
+    statement: 'Pregunta2',
+    answers: [
+      { text: 'respuesta1', for: 'q1AnswerOne', nameAttribute: 'questionTwo', correct: true },
+      { text: 'respuesta2', for: 'q1AnswerTwo', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta3', for: 'q1AnswerThree', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta4', for: 'q1Answerfour', nameAttribute: 'questionTwo', correct: false },
+    ]
+  }, {
+    statement: 'Pregunta2',
+    answers: [
+      { text: 'respuesta1', for: 'q1AnswerOne', nameAttribute: 'questionTwo', correct: true },
+      { text: 'respuesta2', for: 'q1AnswerTwo', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta3', for: 'q1AnswerThree', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta4', for: 'q1Answerfour', nameAttribute: 'questionTwo', correct: false },
+    ]
+  }, {
+    statement: 'Pregunta2',
+    answers: [
+      { text: 'respuesta1', for: 'q1AnswerOne', nameAttribute: 'questionTwo', correct: true },
+      { text: 'respuesta2', for: 'q1AnswerTwo', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta3', for: 'q1AnswerThree', nameAttribute: 'questionTwo', correct: false },
+      { text: 'respuesta4', for: 'q1Answerfour', nameAttribute: 'questionTwo', correct: false },
+    ]
+  },
+];
